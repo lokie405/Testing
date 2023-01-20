36e8ad2e664476be92b2cdfcaff2ef48df5ee258
Velhun Serhiy <lokie405@gmail.com>
2023-01-20 23:19:11 +0200

    Init commit first

---
+++ app/src/main/java/com/example/testsheet/Main.java
@@ -0,0 +1,15 @@
+package com.example.testsheet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main extends Activity {
+int field1;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
